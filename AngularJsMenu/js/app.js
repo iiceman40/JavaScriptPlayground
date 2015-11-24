8216b1ab2d2080b95daf44f065c84974007b5ef5
iiceman40 <sascha.siemer@gmx.de>
2015-11-24 19:08:27 +0100

    playing around with angular

---
+++ AngularJsMenu/js/app.js
@@ -0,0 +1,49 @@
+var app = angular.module('tutorialApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {templateUrl: 'templates/page/articles.html'})
+		.when('/about', {template: 'Über unsere Pizzeria'})
+		.otherwise({redirectTo: '/'});
+});
+
+app.controller('ArticlesCtrl', function ($scope, $http, Cart) {
+	$scope.cart = Cart;
+	$http.get('data/articles.json').then(function (articlesResponse) {
+		$scope.articles = articlesResponse.data;
+	});
+});
+
+app.controller('CartCtrl', function ($scope, Cart) {
+	$scope.cart = Cart;
+});
+
+app.factory('Cart', function () {
+	var items = [];
+	return {
+		getItems: function () {
+			return items;
+		},
+		addArticle: function (article) {
+			items.push(article);
+		},
+		removeArticle: function (index) {
+			items.splice(index, 1);
+		},
+		sum: function () {
+			return items.reduce(function (total, article) {
+				return total + article.price;
+			}, 0);
+		}
+	};
+});
+
+app.directive('price', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			value: '='
+		},
+		templateUrl: 'templates/partials/price.tpl.html'
+	}
+});
